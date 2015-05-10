6b6ff29b5ad13f1f5c3ecd044ef291dbbddf671f
kwmiebach <kwmiebach@gmail.com>
2015-05-10 15:42:03 +0000

    Make a package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='easylog',
+      version='0.1',
+      description='A simple wrapper to create python loggers',
+      url='http://github.com/hbsdev/easylog',
+      author='kwmiebach',
+      author_email='kwmiebach@gmail.com',
+      license='GNU GENERAL PUBLIC LICENSE Version 3',
+      packages=['easylog'],
+      zip_safe=False)
